84bb25b87ee983a597f52995ce511d1330bc0e0e
jperry <jperry2266@gmail.com>
2018-04-04 10:28:51 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jessica Parish on 03/04/2018.
+//  Copyright © 2018 Jessica Parish. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
